2793aabcdb8e1bcdd52b18bb047fc1637bd8aa83
LuckyMan <goodluck3110.dev@gmail.com>
2024-08-06 17:42:00 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TouchableWithoutFeedback, StyleSheet } from 'react-native';
 
 export default function App() {
+  const [bgColor, setBgColor] = useState('#fff'); // Initial background color
+
+  const handlePress = () => {
+    // Generate a random color
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    setBgColor(randomColor);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback onPress={handlePress}>
+      <View style={[styles.container, { backgroundColor: bgColor }]}>
+        <Text>Hello there</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
